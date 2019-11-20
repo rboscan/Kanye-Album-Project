9cd12a1891bd1c750d41b7dba92db6e40cce84b2
rboscan <51136553+rboscan@users.noreply.github.com>
2019-11-20 12:54:07 -0500

    Album Titles/Background Boxes
    Added opaque text boxes to surround album titles and background text in tune with the Roland TR808.

--- web-src/js/kanye.js
+++ web-src/js/kanye.js
@@ -1,16 +1,13 @@
 $('#carousel-body .slick').slick({
     centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3, 
-    // prevArrow:"<img class='a-left control-c prev slick-prev' src='css/images/knob.png'>",
-    // nextArrow:"<img class='a-right control-c next slick-next' src='css/images/knob.png'>",
+    slidesToShow: 3,
+    focusOnSelect: true,
     responsive: [
         {
             breakpoint: 768,
             settings: {
                 arrows: false,
                 centerMode: true,
-                centerPadding: '40px',
                 slidesToShow: 3
             }
         },
@@ -19,7 +16,6 @@ $('#carousel-body .slick').slick({
             settings: {
                 arrows: false,
                 centerMode: true,
-                centerPadding: '40px',
                 slidesToShow: 1
             }
         }
