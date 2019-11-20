b685aadd61aca042fd335c2ae2e58f1357e67074
rboscan <51136553+rboscan@users.noreply.github.com>
2019-11-20 12:03:33 -0500

    Replaced Arrows
    Replaced arrows with 808 knobs

--- web-src/js/kanye.js
+++ web-src/js/kanye.js
@@ -1,8 +1,9 @@
 $('#carousel-body .slick').slick({
     centerMode: true,
     centerPadding: '60px',
-    slidesToShow: 3,
-    infinite:true,
+    slidesToShow: 3, 
+    // prevArrow:"<img class='a-left control-c prev slick-prev' src='css/images/knob.png'>",
+    // nextArrow:"<img class='a-right control-c next slick-next' src='css/images/knob.png'>",
     responsive: [
         {
             breakpoint: 768,
