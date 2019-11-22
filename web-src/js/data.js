8c2e849e7a43b752c3bd77c59614f39911c89059
rboscan <51136553+rboscan@users.noreply.github.com>
2019-11-22 11:21:54 -0500

    HTML Table Filter
    Filter added to HTML table so that it matches album on carousel using D3.js.

---
+++ web-src/js/data.js
@@ -0,0 +1,656 @@
+var data = [{
+    album: "College Dropout",
+    song: "Intro",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "College Dropout",
+    song: "We Don't Care",
+    sentiment_score: "-0.300000012",
+    rating: "Negative"
+}, {
+    album: "College Dropout",
+    song: "Graduation Day",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "College Dropout",
+    song: "All Falls Down",
+    sentiment_score: "-0.600000024",
+    rating: "Negative"
+}, {
+    album: "College Dropout",
+    song: "I'll Fly Away",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "College Dropout",
+    song: "Spaceship",
+    sentiment_score: "-0.5",
+    rating: "Negative"
+}, {
+    album: "College Dropout",
+    song: "Jesus Walks",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "College Dropout",
+    song: "Never Let Me Down",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "College Dropout",
+    song: "Get Em High",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "College Dropout",
+    song: "Workout Plan",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "College Dropout",
+    song: "The New Workout Plan",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "College Dropout",
+    song: "Slow Jamz",
+    sentiment_score: "0.5",
+    rating: "Positive"
+}, {
+    album: "College Dropout",
+    song: "Breathe In Breathe Out",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "College Dropout",
+    song: "School Spirit Skit 1",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "College Dropout",
+    song: "School Spirit",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "College Dropout",
+    song: "School Spirit Skit 2",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "College Dropout",
+    song: "Lil Jimmy Skit",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "College Dropout",
+    song: "Two Words",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "College Dropout",
+    song: "Through The Wire",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "College Dropout",
+    song: "Family Business",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "College Dropout",
+    song: "Last Call",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Late Registration",
+    song: "Wake Up Mr. West",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Heard 'Em Say",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Touch The Sky",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "Late Registration",
+    song: "Gold Digger",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Late Registration",
+    song: "Skit #1",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Drive Slow",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "Late Registration",
+    song: "My Way Home",
+    sentiment_score: "-0.699999988",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Crack Music",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "Late Registration",
+    song: "Roses",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "Late Registration",
+    song: "Bring Me Down",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Addiction",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Skit #2",
+    sentiment_score: "-0.899999976",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Diamonds From Sierra Leone (Remix)",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Late Registration",
+    song: "We Major",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "Late Registration",
+    song: "Skit #3",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Hey Mama",
+    sentiment_score: "0.400000006",
+    rating: "Positive"
+}, {
+    album: "Late Registration",
+    song: "Celebration",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Late Registration",
+    song: "Skit #4",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "Late Registration",
+    song: "Gone",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Late Registration",
+    song: "Diamonds From Sierra Leone",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Late Registration",
+    song: "Late",
+    sentiment_score: "-0.5",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Good Morning",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Graduation",
+    song: "Champion",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Stronger",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Graduation",
+    song: "I Wonder",
+    sentiment_score: "-0.400000006",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Good Life",
+    sentiment_score: "0.400000006",
+    rating: "Positive"
+}, {
+    album: "Graduation",
+    song: "Can't Tell Me Nothing",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Graduation",
+    song: "Barry Bonds",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Graduation",
+    song: "Drunk & Hot Girls",
+    sentiment_score: "-0.5",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Flashing Lights",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Everything I Am",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "The Glory",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Graduation",
+    song: "Homecoming",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Graduation",
+    song: "Big Brother",
+    sentiment_score: "-0.600000024",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Good Night",
+    sentiment_score: "-0.600000024",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Bittersweet Poetry",
+    sentiment_score: "-0.5",
+    rating: "Negative"
+}, {
+    album: "Graduation",
+    song: "Can't Tell Me Nothing (Remix)",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "808s & Heartbreak",
+    song: "Say You Will",
+    sentiment_score: "-0.699999988",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "Welcome To Heartbreak",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "808s & Heartbreak",
+    song: "Heartless",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "808s & Heartbreak",
+    song: "Amazing",
+    sentiment_score: "0.899999976",
+    rating: "Positive"
+}, {
+    album: "808s & Heartbreak",
+    song: "Love Lockdown",
+    sentiment_score: "-0.5",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "Paranoid",
+    sentiment_score: "-0.800000012",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "RoboCop",
+    sentiment_score: "-0.300000012",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "Street Lights",
+    sentiment_score: "-0.300000012",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "Bad News",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "See You In My Nightmares",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "Coldest Winter",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "808s & Heartbreak",
+    song: "Pinocchio Story",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Dark Fantasy",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Gorgeous",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Power",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "All Of The Lights",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Monster",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "So Appalled",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Devil In A New Dress",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Runaway",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Hell Of A Life",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Blame Game",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Lost In The World",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "Who Will Survive In America",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "My Beautiful Dark Twisted Fantasy",
+    song: "See Me Now",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "Yeezus",
+    song: "On Sight",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "Black Skinhead",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "I Am A God",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "New Slaves",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "Hold My Liquor",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "I'm In It",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "Blood On The Leaves",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "Guilt Trip",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "Yeezus",
+    song: "Send It Up",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Yeezus",
+    song: "Bound 2",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Ultralight Beam",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "Father Stretch My Hands Pt. 1",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "Father Stretch My Hands Pt. 2",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Famous",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "Feedback",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Low Lights",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "Highlights",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Freestyle 4",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "I Love Kanye",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "Waves",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "FML",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Real Friends",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Wolves",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Frank's Track",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "Siiiiiiiiilver Surffffeeeeer Intermission",
+    sentiment_score: "0.400000006",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "30 Hours",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "No More Parties In LA",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Facts (Charlie Heat Version)",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "The Life Of Pablo",
+    song: "Fade",
+    sentiment_score: "0.200000003",
+    rating: "Positive"
+}, {
+    album: "The Life Of Pablo",
+    song: "Saint Pablo",
+    sentiment_score: "0.100000001",
+    rating: "Positive"
+}, {
+    album: "Ye",
+    song: "I Thought About Killing You",
+    sentiment_score: "-0.400000006",
+    rating: "Negative"
+}, {
+    album: "Ye",
+    song: "Yikes",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Ye",
+    song: "All Mine",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Ye",
+    song: "Wouldn't Leave",
+    sentiment_score: "-0.5",
+    rating: "Negative"
+}, {
+    album: "Ye",
+    song: "No Mistakes",
+    sentiment_score: "0.300000012",
+    rating: "Positive"
+}, {
+    album: "Ye",
+    song: "Ghost Town",
+    sentiment_score: "-0.100000001",
+    rating: "Negative"
+}, {
+    album: "Ye",
+    song: "Violent Crimes",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, {
+    album: "Jesus is King",
+    song: "Every Hour",
+    sentiment_score: "0.300000012",
+    rating: "Positive"
+}, {
+    album: "Jesus is King",
+    song: "Selah",
+    sentiment_score: "0.5",
+    rating: "Positive"
+}, {
+    album: "Jesus is King",
+    song: "Follow God",
+    sentiment_score: "0.400000006",
+    rating: "Positive"
+}, {
+    album: "Jesus is King",
+    song: "Closed on Sunday",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "Jesus is King",
+    song: "On God",
+    sentiment_score: "-0.200000003",
+    rating: "Negative"
+}, {
+    album: "Jesus is King",
+    song: "Everything We Need",
+    sentiment_score: "0.300000012",
+    rating: "Positive"
+}, {
+    album: "Jesus is King",
+    song: "Water",
+    sentiment_score: "0.300000012",
+    rating: "Positive"
+}, {
+    album: "Jesus is King",
+    song: "God Is",
+    sentiment_score: "0.400000006",
+    rating: "Positive"
+}, {
+    album: "Jesus is King",
+    song: "Hands On",
+    sentiment_score: "-0.400000006",
+    rating: "Negative"
+}, {
+    album: "Jesus is King",
+    song: "Use This Gospel",
+    sentiment_score: "0.400000006",
+    rating: "Positive"
+}, {
+    album: "Jesus is King",
+    song: "Jesus is Lord",
+    sentiment_score: "0",
+    rating: "Neutral"
+}, ]
