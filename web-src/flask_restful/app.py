402613a1aaca697241e41f12fc944dc81f38c92d
rboscan <51136553+rboscan@users.noreply.github.com>
2019-11-22 23:12:43 -0500

    Rudimentary Flask
    Flask server complete with working dictionary, minor details needed on website and template.

--- web-src/flask_restful/app.py
+++ web-src/flask_restful/app.py
@@ -6,13 +6,12 @@ from flask import (
     request)
 import pandas as pd
 import os
-import csv
 from flask_sqlalchemy import SQLAlchemy
 
 # Innit app
 app = Flask(__name__)
 password = os.environ.get('DB_PASS')
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:7410733Rafael$@localhost/Kanye"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:"+password+"@localhost/Kanye"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 db = SQLAlchemy(app)
@@ -29,25 +28,46 @@ class Discography(db.Model):
     def __repr__(self):
         return '<Discography %r>' % (self.album)
 
+@app.before_first_request
+def setup():
+    db.drop_all()
+    db.create_all()
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    data = []
-    if request.method == 'GET':
-        database = pd.read_csv('wipcsv.csv')
-        json_ = database.to_dict()
-        album_ind = json_["Album"]
-        rating_ind = json_["Rating"]
-        sentiment_ind = json_["Sentiment"]
-        song_ind = json_["Song"]
-        for row in json_:
-            i = 0
-            discography = Discography(album=album_ind[i], song=song_ind[i], sentiment=sentiment_ind[i], rating=rating_ind[i])
-            i = i+1
-            db.session.add(discography)
-            db.session.commit()
-        return "complete"
+    return render_template('index.html')
 
+@app.route("/process", methods=['GET', 'POST'])
+def process():
+    if request.method == 'GET':
+        
+        db_csv = pd.read_csv('wipcsv.csv')
+        json_ = db_csv.to_dict()
+        
+        for i in range(0,131,1):
+            discography = Discography(
+            album=json_['Album'][i],
+            song=json_['Song'][i],
+            sentiment=json_['Sentiment'][i],
+            rating=json_['Rating'][i])
+            db.session.add(discography)
+
+        db.session.commit()
+    return render_template("form.html")
+
+@app.route("/result")
+def list_hits():
+    results = db.session.query(Discography.album, Discography.song, Discography.sentiment, Discography.rating).all()
+
+    hits = []
+    for result in results:
+        hits.append({
+            "Album": result[0],
+            "Song": result[1],
+            "Sentiment":result[2],
+            "Rating":result[3]
+        })
+    return jsonify(hits)
 
 # Run Server
 if __name__ == '__main__':
