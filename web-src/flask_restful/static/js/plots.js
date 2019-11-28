f2a0682f3a309a61372140438943dfd915609341
rboscan <51136553+rboscan@users.noreply.github.com>
2019-11-28 00:31:49 -0500

    graph colors

--- web-src/flask_restful/static/js/plots.js
+++ web-src/flask_restful/static/js/plots.js
@@ -2,94 +2,9 @@ var tbody = d3.select("tbody");
 
 d3.json('/result', function (jsondata) {
     // PLOT 1 DICTS AND NECESSARY VARIABLES
-
     // ACTIVE SLIDE IMAGE
     let inputElement = d3.select("div.slick-slide.slick-current.slick-active.slick-center").select("div").select("div").select("img").attr("id");
-    var song_name = [];
-    var album_name = [];
-    var positive = [];
-    var negative = [];
-    var neutral = [];
-    // PLOT 1 START
-    for (var i = 0; i < jsondata.length; i++) {
-        if (jsondata[i]['Album'] == inputElement) {
-            var album = jsondata[i]['Album'];
-            album_name.push(album);
-            var song_sentiment = jsondata[i]['Sentiment'];
-            song_name.push(jsondata[i]['Song']);
-            if (song_sentiment > 0) {
-                positive.push(song_sentiment);
-            } else if (song_sentiment === 0) {
-                neutral.push(song_sentiment);
-            } else {
-                negative.push(song_sentiment);
-            }
-        };
-    };
-    pos_len = positive.length
-    neg_len = negative.length
-    neut_len = neutral.length
-    var pos = {
-        x: [pos_len],
-        y: ['Bar'],
-        name: 'Positive',
-        orientation: 'h',
-        marker: {
-            color: 'rgba(243, 216, 112, 0.9)',
-            width: 1
-        },
-        type: 'bar'
-    };
-
-    var neg = {
-        x: [neg_len],
-        y: ['Bar'],
-        name: 'Negative',
-        orientation: 'h',
-        marker: {
-            color: 'rgba(236, 112, 86, 0.9)',
-            width: 1
-        },
-        type: 'bar'
-    };
-
-    var neut = {
-        x: [neut_len],
-        y: ['Bar'],
-        name: 'Neutral',
-        orientation: 'h',
-        marker: {
-            color: 'rgba(244, 181, 116, 0.9)',
-            width: 1
-        },
-        type: 'bar'
-    };
-
-    var data = [neg, neut, pos];
-
-    var layout = {
-        title: {
-            text: "Datum Distribution",
-        },
-        xaxis: {
-            title: {
-                text: "Sentiment Score",
-            },
-        },
-        yaxis: {
-            title: {
-                text: "Sentiment Distribution by Count",
-            },
-        },
-        barmode: 'stack',
-        paper_bgcolor: 'rgba(0,0,0,0)',
-        plot_bgcolor: 'rgba(0,0,0,0)'
-    };
-
-    Plotly.newPlot('plot1', data, layout, { responsive: true });
-
     // SLICK SLIDE 2ND PLOT
-
     var song_name2 = [];
     var positive2 = [];
     var negative2 = [];
@@ -112,13 +27,24 @@ d3.json('/result', function (jsondata) {
             var data2 = [{
                 values: [pos_len2, neut_len2, neg_len2],
                 labels: ['Positive', 'Neutral', 'Negative'],
+                marker:{
+                    colors: ['#F5D772','#F0A775','#ED6D59']
+                },
                 type: 'pie',
                 hole: 0.6
             }];
 
             var layout2 = {
+                legend: {
+                    font: {
+                        color:'rgb(228, 117, 59)'
+                    }
+                },
                 title: {
                     text: 'Datum Distribution',
+                    font: {
+                        color:'rgb(228, 117, 59)'
+                    }
                 },
                 paper_bgcolor: 'rgba(0,0,0,0)',
                 plot_bgcolor: 'rgba(0,0,0,0)'
@@ -132,91 +58,8 @@ d3.json('/result', function (jsondata) {
 // SLICK SLIDE CHANGE
 $('div').on("afterChange", function () {
     d3.json('/result', function (jsondata) {
-        var song_name = [];
-        var positive = [];
-        var negative = [];
-        var neutral = [];
-        var album_name = [];
         let inputElement = d3.select("div.slick-slide.slick-current.slick-active.slick-center").select("div").select("div").select("img").attr("id");
-        for (var i = 0; i < jsondata.length; i++) {
-            var album = jsondata[i]['Album'];
-            album_name.push(album);
-            if (jsondata[i]['Album'] == inputElement) {
-                var song_sentiment = jsondata[i]['Sentiment'];
-                song_name.push(jsondata[i]['Song']);
-                if (song_sentiment > 0) {
-                    positive.push(song_sentiment);
-                } else if (song_sentiment === 0) {
-                    neutral.push(song_sentiment);
-                } else {
-                    negative.push(song_sentiment);
-                }
-            };
-        };
-        pos_len = positive.length
-        neg_len = negative.length
-        neut_len = neutral.length
-        var pos = {
-            x: [pos_len],
-            y: ['Bar'],
-            name: 'Positive',
-            orientation: 'h',
-            marker: {
-                color: 'rgba(243, 216, 112, 0.9)',
-                width: 1
-            },
-            type: 'bar'
-        };
-
-        var neg = {
-            x: [neg_len],
-            y: ['Bar'],
-            name: 'Negative',
-            orientation: 'h',
-            marker: {
-                color: 'rgba(236, 112, 86, 0.9)',
-                width: 1
-            },
-            type: 'bar'
-        };
-
-        var neut = {
-            x: [neut_len],
-            y: ['Bar'],
-            name: 'Neutral',
-            orientation: 'h',
-            marker: {
-                color: 'rgba(244, 181, 116, 0.9)',
-                width: 1
-            },
-            type: 'bar'
-        };
-
-        var data = [neg, neut, pos];
-
-        var layout = {
-            title: {
-                text: 'Datum Distribution',
-            },
-            xaxis: {
-                title: {
-                    text: "Sentiment Score",
-                },
-            },
-            yaxis: {
-                title: {
-                    text: "Sentiment Distribution by Count",
-                },
-            },
-            paper_bgcolor: 'rgba(0,0,0,0)',
-            plot_bgcolor: 'rgba(0,0,0,0)',
-            barmode: 'stack'
-        };
-
-        Plotly.newPlot('plot1', data, layout, { responsive: true });
-
         // PLOT 2 SLICK SLIDE CHANGE
-
         var song_name2 = [];
         var positive2 = [];
         var negative2 = [];
@@ -240,12 +83,24 @@ $('div').on("afterChange", function () {
                     values: [pos_len2, neut_len2, neg_len2],
                     labels: ['Positive', 'Neutral', 'Negative'],
                     type: 'pie',
+                    marker:{
+                        colors: ['#F5D772','#F0A775','#ED6D59']
+                    },
                     hole: 0.6
                 }];
 
                 var layout2 = {
+                    
+                    legend: {
+                        font: {
+                            color:'rgb(228, 117, 59)'
+                        }
+                    },
                     title: {
                         text: 'Datum Distribution',
+                        font: {
+                            color:'rgb(228, 117, 59)'
+                        }
                     },
                     paper_bgcolor: 'rgba(0,0,0,0)',
                     plot_bgcolor: 'rgba(0,0,0,0)'
