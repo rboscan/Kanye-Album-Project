805df7f365e19847ecf5a9611c766e1eddea812f
rboscan <51136553+rboscan@users.noreply.github.com>
2019-12-01 12:18:39 -0500

    Playcount and Based Bar Chart
    Added playcount functionality to sunburst and a based bar chart to show sentiment score distribution for each album

--- web-src/flask_restful/app.py
+++ web-src/flask_restful/app.py
@@ -4,6 +4,8 @@ from flask import (
     render_template,
     jsonify,
     request)
+import json
+import requests
 import pandas as pd
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -11,7 +13,6 @@ from flask_restful import Resource, Api
 
 # Innit app
 app = Flask(__name__)
-password = os.environ.get('DB_PASS')
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:postgres@localhost/Kanye"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -25,12 +26,14 @@ class Discography(db.Model):
     song = db.Column(db.String(100))
     sentiment = db.Column(db.Float)
     rating = db.Column(db.String(50))
+    playcount = db.Column(db.Float)
 
-    def __init__(self, album, song, sentiment, rating):
+    def __init__(self, album, song, sentiment, rating, playcount):
         self.album = album
         self.song = song
         self.sentiment = sentiment
         self.rating = rating
+        self.playcount = playcount
 
     def __repr__(self):
         return '<Discography %r>' % (self.album)
@@ -42,15 +45,48 @@ def setup():
     db.create_all()
     if request.method == 'GET':
 
+        albums = [
+            "3ff2p3LnR6V7m6BinwhNaQ",
+            "5ll74bqtkcXlKE7wwkMq4g",
+            "3SZr5Pco2oqKFORCP3WNj9",
+            "3WFTGIO6E3Xh4paEOBY9OU",
+            "20r762YmB5HeofjMCiPMLv",
+            "7D2NdGvBHIavgLhmcwhluK",
+            "7gsWAHLeT0w7es6FofOXk1",
+            "2Ek1q2haOnxVqhvVKqMvJe",
+            "0FgZKfoU2Br5sHOfvZKTI9"
+        ]
+
+        playcount = []
+        song = []
+        for album in albums:
+            i=0
+            url=f"https://t4ils.dev:4433/api/beta/albumPlayCount/?albumid={album}"
+            request_url = requests.get(url)
+            request_json=request_url.json()
+            song_name = request_json['data']
+            i=0
+            for i in range(0,len(song_name)):
+                playcount.append(request_json['data'][i]['playcount'])
+                song.append(request_json['data'][i]['name'])
+                i=i+1
+        playcount_df = pd.DataFrame({'Song':song, 'Playcount':playcount})
+        playcount_df.replace({"Pt. 2":"Father Stretch My Hands Pt. 2"}, value=None, inplace=True)
         db_csv = pd.read_csv('wipcsv.csv')
+        playcount_song = db_csv.merge(playcount_df, how='outer',on='Song')
+        playcount_song['Playcount'] = playcount_song['Playcount'].fillna(0)
+        playcount_song = playcount_song.dropna().reset_index(drop=True)
+        db_csv = playcount_song
         json_ = db_csv.to_dict()
 
         for i in range(0, 131, 1):
             discography = Discography(
                 album=json_['Album'][i],
-                song=json_['Song'][i],
+                rating=json_['Rating'][i],
                 sentiment=json_['Sentiment'][i],
-                rating=json_['Rating'][i])
+                song=json_['Song'][i],
+                playcount=json_['Playcount'][i])
+            
             db.session.add(discography)
 
         db.session.commit()
@@ -60,14 +96,15 @@ def setup():
 @app.route('/result', methods=['GET', 'POST'])
 def process():
     if request.method == 'GET':
-        results = db.session.query(Discography.album, Discography.song, Discography.sentiment, Discography.rating).all()
+        results = db.session.query(Discography.album, Discography.song, Discography.sentiment, Discography.rating, Discography.playcount).all()
         hits = []
         for result in results:
             hits.append({
                 "Album": result[0],
                 "Song": result[1],
                 "Sentiment": result[2],
-                "Rating": result[3]
+                "Rating": result[3],
+                "Playcount":result[4]
             })
 
     return jsonify(hits)
@@ -75,7 +112,7 @@ def process():
 @app.route('/jsontree', methods=['GET','POST'])
 def tree():
     if request.method == 'GET':
-        results = db.session.query(Discography.album, Discography.song, Discography.sentiment, Discography.rating).all()
+        results = db.session.query(Discography.album, Discography.song, Discography.sentiment, Discography.rating, Discography.playcount).all()
         al_unique = {'name':'Discography', 'children':[]}
         for result in results:
             if {'name':result[0],'children':[]} not in al_unique['children']:
@@ -83,14 +120,14 @@ def tree():
         i=1
         for result in results:
             if al_unique['children'][i-1]['name'] == result[0]:
-                al_unique['children'][i-1]['children'].append({'name':result[1],'children':[{'name':result[2],'value':100},{'name':result[3],'value':100}]})
+                al_unique['children'][i-1]['children'].append({'name':result[1],'children':[{'name':result[2],'value':100},{'name':result[3],'value':result[4]}]})
 
             else: i=i+1
     return jsonify(al_unique)
 
-@app.route('/test')
+@app.route('/sunburst')
 def test():
-    return render_template('test.html')
+    return render_template('sunburst.html')
 
 @app.route('/')
 def index():
--- web-src/flask_restful/static/js/plots.js
+++ web-src/flask_restful/static/js/plots.js
@@ -3,28 +3,40 @@ var tbody = d3.select("tbody");
 d3.json('/result', function (jsondata) {
     // PLOT 1 DICTS AND NECESSARY VARIABLES
     // ACTIVE SLIDE IMAGE
+
     let inputElement = d3.select("div.slick-slide.slick-current.slick-active.slick-center").select("div").select("div").select("img").attr("id");
-    var song_name2 = [];
-    var positive2 = [];
-    var negative2 = [];
-    var neutral2 = [];
+
+    var song_name_pos = [];
+    var song_name_neg = [];
+    var song_name_neut = [];
+    var positive = [];
+    var negative = [];
+    var neutral = [];
+
     for (var i = 0; i < jsondata.length; i++) {
         if (jsondata[i]['Album'] == inputElement) {
-            var song_sentiment = jsondata[i]['Sentiment'];
-            song_name2.push(jsondata[i]['Song']);
-            if (song_sentiment > 0) {
-                positive2.push(song_sentiment);
-            } else if (song_sentiment === 0) {
-                neutral2.push(song_sentiment);
-            } else {
-                negative2.push(song_sentiment);
-            }
-            pos_len2 = positive2.length;
-            neut_len2 = neutral2.length;
-            neg_len2 = negative2.length;
 
-            var data2 = [{
-                values: [pos_len2, neut_len2, neg_len2],
+            var song_sentiment = jsondata[i]['Sentiment'];
+
+            var song_name = jsondata[i]['Song'];
+
+            if (song_sentiment > 0) {
+                positive.push(song_sentiment);
+                song_name_pos.push(song_name);
+            } else if (song_sentiment === 0) {
+                neutral.push(song_sentiment);
+                song_name_neut.push(song_name);
+            } else {
+                negative.push(song_sentiment);
+                song_name_neg.push(song_name);
+            }
+
+            var pos_len = positive.length;
+            var neut_len = neutral.length;
+            var neg_len = negative.length;
+
+            var data = [{
+                values: [pos_len, neut_len, neg_len],
                 labels: ['Positive', 'Neutral', 'Negative'],
                 marker: {
                     colors: ['#F5D772', '#F0A775', '#ED6D59']
@@ -33,7 +45,7 @@ d3.json('/result', function (jsondata) {
                 hole: 0.6
             }];
 
-            var layout2 = {
+            var layout = {
                 legend: {
                     font: {
                         color: 'rgb(228, 117, 59)'
@@ -45,55 +57,105 @@ d3.json('/result', function (jsondata) {
                         color: 'rgb(228, 117, 59)'
                     }
                 },
+                yaxis: {
+                    textfont: {
+                        color: '#ff7f0e'
+                    }
+                },
                 paper_bgcolor: 'rgba(0,0,0,0)',
                 plot_bgcolor: 'rgba(0,0,0,0)'
             };
-            Plotly.newPlot('plot2', data2, layout2, {
+            Plotly.newPlot('plot1', data, layout, {
                 responsive: true
+            });
 
+            var data2 = [{
+                    type: 'bar',
+                    x: song_name_pos,
+                    y: positive,
+                    base: [positive],
+                    marker: {
+                        color: 'blue'
+                    },
+                    name: 'positive'
+                },
+                {
+                    type: 'bar',
+                    x: song_name_neut,
+                    y: neutral,
+                    base: [neutral],
+                    marker: {
+                        color: 'yellow'
+                    },
+                    name: 'neutral'
+                },
+                {
+                    type: 'bar',
+                    x: song_name_neg,
+                    y: negative,
+                    base: [negative],
+                    marker: {
+                        color: 'red'
+                    },
+                    name: 'negative'
+                }
+            ]
 
+            Plotly.newPlot('plot2', data2, layout, {
+                responsive: true
             });
         };
     };
-
 });
 
 // SLICK SLIDE CHANGE
 $('div').on("afterChange", function () {
     d3.json('/result', function (jsondata) {
+        // PLOT 1 DICTS AND NECESSARY VARIABLES
+        // ACTIVE SLIDE IMAGE
+
         let inputElement = d3.select("div.slick-slide.slick-current.slick-active.slick-center").select("div").select("div").select("img").attr("id");
-        // PLOT 2 SLICK SLIDE CHANGE
-        var song_name2 = [];
-        var positive2 = [];
-        var negative2 = [];
-        var neutral2 = [];
+
+        var song_name_pos = [];
+        var song_name_neg = [];
+        var song_name_neut = [];
+        var positive = [];
+        var negative = [];
+        var neutral = [];
+
         for (var i = 0; i < jsondata.length; i++) {
             if (jsondata[i]['Album'] == inputElement) {
-                var song_sentiment2 = jsondata[i]['Sentiment'];
-                song_name2.push(jsondata[i]['Song']);
-                if (song_sentiment2 > 0) {
-                    positive2.push(song_sentiment2);
-                } else if (song_sentiment2 === 0) {
-                    neutral2.push(song_sentiment2);
-                } else {
-                    negative2.push(song_sentiment2);
-                }
-                pos_len2 = positive2.length;
-                neut_len2 = neutral2.length;
-                neg_len2 = negative2.length;
 
-                var data2 = [{
-                    values: [pos_len2, neut_len2, neg_len2],
+                var song_sentiment = jsondata[i]['Sentiment'];
+
+                var song_name = jsondata[i]['Song'];
+
+                if (song_sentiment > 0) {
+                    positive.push(song_sentiment);
+                    song_name_pos.push(song_name);
+                } else if (song_sentiment === 0) {
+                    neutral.push(song_sentiment);
+                    song_name_neut.push(song_name);
+                } else {
+                    negative.push(song_sentiment);
+                    song_name_neg.push(song_name);
+                }
+
+                var pos_len = positive.length;
+                var neut_len = neutral.length;
+                var neg_len = negative.length;
+
+                var data = [{
+                    values: [pos_len, neut_len, neg_len],
                     labels: ['Positive', 'Neutral', 'Negative'],
-                    type: 'pie',
                     marker: {
                         colors: ['#F5D772', '#F0A775', '#ED6D59']
                     },
+                    type: 'pie',
                     hole: 0.6
                 }];
 
-                var layout2 = {
-
+                var layout = {
                     legend: {
                         font: {
                             color: 'rgb(228, 117, 59)'
@@ -108,11 +170,46 @@ $('div').on("afterChange", function () {
                     paper_bgcolor: 'rgba(0,0,0,0)',
                     plot_bgcolor: 'rgba(0,0,0,0)'
                 };
-                Plotly.newPlot('plot2', data2, layout2, {
+                Plotly.newPlot('plot1', data, layout, {
+                    responsive: true
+                });
+
+                var data2 = [{
+                        type: 'bar',
+                        x: song_name_pos,
+                        y: positive,
+                        base: [positive],
+                        marker: {
+                            color: 'blue'
+                        },
+                        name: 'positive'
+                    },
+                    {
+                        type: 'bar',
+                        x: song_name_neut,
+                        y: neutral,
+                        base: [neutral],
+                        marker: {
+                            color: 'yellow'
+                        },
+                        name: 'neutral'
+                    },
+                    {
+                        type: 'bar',
+                        x: song_name_neg,
+                        y: negative,
+                        base: [negative],
+                        marker: {
+                            color: 'red'
+                        },
+                        name: 'negative'
+                    }
+                ]
+
+                Plotly.newPlot('plot2', data2, layout, {
                     responsive: true
                 });
             };
         };
-
     });
 });
